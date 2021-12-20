22adc6c42c004087c7a9eae4f6a5fadd65ba53c0
vinay-jtc <vinay.goyal@jalantechnologies.com>
2021-12-20 12:05:00 +0530

    Finished Gatsby Tutorial Part 2

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/home/vanni/jtc/gatsby-demo/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/vanni/jtc/gatsby-demo/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/home/vanni/jtc/gatsby-demo/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/home/vanni/jtc/gatsby-demo/src/pages/index.js"))
 }
 
---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle='About page'>
+        <p>Here is about the site!!</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
